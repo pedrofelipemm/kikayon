4856a1cd90ee99262254b48917d566b3e70229d3
Pedro Felipe Marques Moreira <pedrofelipemm@gmail.com>
2016-09-05 21:31:44 -0300

    Basic theme setup

--- app/src/main/java/pmoreira/kikayon/MainActivity.java
+++ app/src/main/java/pmoreira/kikayon/MainActivity.java
@@ -1,7 +1,11 @@
 package pmoreira.kikayon;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +14,32 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        
+        setSupportActionBar((Toolbar) findViewById(R.id.app_bar));
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true); // TODO: 05/09/16 DELEETE THIS SHIT
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true); // TODO: 05/09/16 DELEETE THIS SHIT
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(final Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(final MenuItem item) {
+
+        if (item.getItemId() == R.id.action_settings) {
+            Toast.makeText(this, "Test", Toast.LENGTH_SHORT).show();
+            return true;
+        } else if (item.getItemId() == android.R.id.home) {
+            Toast.makeText(this, "Home", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
